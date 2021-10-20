bb8e1ae70e5265286f5444251bd526e3e1be10cd
Ivan Mitkov <ivan.mitkov@gmail.com>
2021-10-20 11:13:37 +0300

    basic structure

---
+++ src/store/store.js
@@ -0,0 +1,15 @@
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { weatherForecastReducer } from "./reducers/weatherForecastReducer";
+
+const reducer = combineReducers({ weather: weatherForecastReducer });
+const initialState = {};
+const middleware = [thunk];
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
