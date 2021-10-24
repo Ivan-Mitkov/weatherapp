fe36570963019be0cebfb378550fd2fea265d74d
Ivan Mitkov <ivan.mitkov@gmail.com>
2021-10-24 15:24:25 +0300

    real data not json file

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import * as data from "../../exampleCall.json";
+// import * as data from "../../exampleCall.json";
 import {
   WEATHER_DETAILS_REQUEST,
   WEATHER_DETAILS_SUCCESS,
@@ -16,13 +16,13 @@ export const getWeatherDetail =
   (lat = 42.697708, lon = 23.321867) =>
   async (dispatch) => {
     try {
-      // dispatch({ type: WEATHER_DETAILS_REQUEST });
-      // const { data } = await axios.get(
-      //   `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&appid=${process?.env?.REACT_APP_API_KEY}`
-      // );
-      // dispatch({ type: WEATHER_DETAILS_SUCCESS, payload: data });
+      dispatch({ type: WEATHER_DETAILS_REQUEST });
+      const { data } = await axios.get(
+        `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&appid=${process?.env?.REACT_APP_API_KEY}`
+      );
+      dispatch({ type: WEATHER_DETAILS_SUCCESS, payload: data });
       // console.log(data);
-      dispatch({ type: WEATHER_DETAILS_SUCCESS, payload: data.default });
+      // dispatch({ type: WEATHER_DETAILS_SUCCESS, payload: data.default });
     } catch (error) {
       dispatch({
         type: WEATHER_DETAILS_FAIL,
